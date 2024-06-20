7818263a8974a2c949b188f5a9a18b04f033ccbe
art2m <91477956+7aaassss@users.noreply.github.com>
2024-06-20 20:12:50 +0300

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,17 +1,13 @@
 from flask import Flask
 from app.config import Config
-from flask_sqlalchemy import SQLAlchemy
-from flask_migrate import Migrate
+
 
 
 app = Flask(__name__)
 app.config.from_object(Config)
 
-db = SQLAlchemy(app)
-migrate = Migrate(app, db)
 
 # Импорт моделей и api
-from app import models
 from app import routes
 from app import ml
 if __name__ == "__main__":
