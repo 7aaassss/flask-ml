1ec016cbe938bb1631a5f2ccbf5b72389cce19db
KirillBad <kirillbad2004@yandex.ru>
2024-11-27 21:47:06 +0300

    new interface + livestream

--- app/forms.py
+++
@@ -1,8 +0,0 @@
-from flask_wtf import FlaskForm
-from wtforms import FileField, SubmitField, SelectField
-from wtforms.validators import DataRequired
-
-class ImageForm(FlaskForm):
-    file = FileField('File', validators=[DataRequired()])
-    methodSelect = SelectField('Method', choices=[('DETR', 'DETR'), ('YOLO', 'YOLO')], validators=[DataRequired()])
-    submit = SubmitField('Upload')
--- app/routes.py
+++ app/routes.py
@@ -1,49 +1,58 @@
 import os
-from flask import request, flash, render_template, jsonify
-from app.forms import ImageForm
-from app import app, ml
+from flask import render_template, Response, request, jsonify, send_from_directory
+from app import app
+from .object_detection import object_detection_handler
 import uuid
 
-ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif'}
+ALLOWED_EXTENSIONS = {"png", "jpg", "jpeg", "gif"}
+
 
 def uid():
-        return str(uuid.uuid4())
+
+    return str(uuid.uuid4())
+
 
 def allowed_file(filename):
-    return '.' in filename and \
-           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+    return "." in filename and filename.rsplit(".", 1)[1].lower() in ALLOWED_EXTENSIONS
 
-@app.route('/', methods=['POST', 'GET'])
+
+@app.route("/", methods=["POST", "GET"])
 def index():
-    form = ImageForm()
+    return render_template("index.html")
 
-    if request.method == 'POST':
-        if form.validate_on_submit():
-            if 'file' not in request.files:
-                flash('Файл не был выбран!', 'error')
-                return jsonify({'status': 'error', 'message': 'Файл не был выбран!'})
 
-            file = request.files['file']
+@app.route("/about")
+def about():
+    return render_template("about.html")
 
-            if file.filename == '':
-                flash('Имя файла не указано!', 'error')
-                return jsonify({'status': 'error', 'message': 'Имя файла не указано!'})
 
-            if file and allowed_file(file.filename):
-                    filename = uid() + file.filename[-4:]
-                    filepath = os.path.join(os.path.dirname(__file__), 'static', filename)
-                    file.save(filepath)
-                    selected_method = request.form['methodSelect']
-                    if selected_method == 'DETR':
-                            obr = ml.show_detected_image(filepath, 'd')
-                    elif selected_method == 'YOLO':
-                            obr = ml.show_detected_image(filepath, 'y')
-                    flash('GOOd', 'success')
-                    return render_template('image.html', filename=obr)
-            flash('Недопустимый тип файла!', 'error')
-            return jsonify({'status': 'error', 'message': 'Bad type!'})
+@app.route("/object-detection", methods=["POST", "GET"])
+def object_detection():
+    content_type = request.form.get("content")
+    model = request.form.get("model")
+    file = request.files.get("file")
 
-        flash('Ошибка валидации формы!', 'error')
-        return jsonify({'status': 'error', 'message': 'Smth wrong with form!'})
+    if content_type == "image":
+        return jsonify(object_detection_handler.handle_image(model, file))
+    elif content_type == "video":
+        input_path, filename = object_detection_handler.save_file(file)
+        return jsonify({"video_id": filename, "model": model, "content_type": "video"})
+    elif content_type == "camera":
+        return jsonify({"video_id": 0, "model": model, "content_type": "camera"})
 
-    return render_template('index.html', form=form)
+
+@app.route("/video_feed/<video_id>")
+def video_feed(video_id):
+    def generate():
+        return object_detection_handler.process_video(
+            video_id, request.args.get("model"), request.args.get("content_type")
+        )
+
+    return Response(generate(), mimetype="multipart/x-mixed-replace; boundary=frame")
+
+
+@app.route("/media/output/<path:filename>")
+def media_files(filename):
+    return send_from_directory(
+        os.path.join(os.path.dirname(__file__), "media/output"), filename
+    )
